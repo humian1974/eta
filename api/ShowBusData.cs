b67df93c722c040874bb7c5a6cbd6ff63eeadccf
humian1974 <humian1974@outlook.com>
2021-07-14 22:30:22 +0800

    add api para

--- api/ShowBusData.cs
+++ api/ShowBusData.cs
@@ -25,19 +25,37 @@ namespace ShowBusData
         [FunctionName("ShowBusData")]
         public static async Task<IActionResult> ShowBusData([HttpTrigger("get", Route = "ap-data")] HttpRequest req, ILogger log)
         {                              
-            int count = 0;
-            char initial = '0';
+            int count = 10;
+            char initial = '%';
+            string s_sort = "rd";
+            string sort = "elevation desc";
 
             Int32.TryParse(req.Query["c"], out count);
             Char.TryParse(req.Query["i"], out initial);
+            switch (s_sort = req.Query["s"])
+            {
+                case "rd":
+                    sort = "rwy desc";
+                    break;
+                case "ra":
+                    sort = "rwy asc";
+                    break;
+                case "ed":
+                    sort = "elevation desc";
+                    break;
+                case "ea":
+                    sort = "elevation asc";
+                    break;
+            }
             
             using(var conn = new SqlConnection(AZURE_CONN_STRING))
             {
                 var result = await conn.QuerySingleOrDefaultAsync<string>(
-                    "web.GetairportData2", 
+                    "web.GetairportData3", 
                     new {
                         @countn = count,
-                        @initchar = initial
+                        @initchar = initial,
+                        @sort = sort
                     }, commandType: CommandType.StoredProcedure);                
                 
                 return new OkObjectResult(JObject.Parse(result));
