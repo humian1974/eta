105c5280f90c2e9499881643a3e9f31b7cb09f42
xy01dv2 <xy01@0xx2m.onmicrosoft.com>
2023-12-03 16:29:47 +0800

    rwy surface

--- api/GetAPdata.cs
+++ api/GetAPdata.cs
@@ -62,7 +62,7 @@ namespace GetAPdata
             using(var conn = new SqlConnection(AZURE_CONN_STRING))
             {
                 var result = await conn.QuerySingleOrDefaultAsync<string>(
-                    "web.GetairportData4", 
+                    "web.GetairportData2", 
                     new {
                         @countn = count,
                         @ident =ident,
